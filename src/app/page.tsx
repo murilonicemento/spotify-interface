9ff1a82ec16e0dfb4ded0ee2bdc5251b60357706
murilonicemento <murilo_nascimento22@outlook.com>
2023-06-10 15:44:55 -0300

    Separação em componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import { Footer } from "@/components/Footer";
+import { Sidebar } from "@/components/Sidebar";
 import {
-  Home as HomeIcon,
-  Search,
-  Library,
   ChevronLeft,
   ChevronRight,
   Play,
@@ -21,57 +20,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <button className="w-3 h-3 bg-red-500 rounded-full"></button>
-            <button className="w-3 h-3 bg-yellow-500 rounded-full"></button>
-            <button className="w-3 h-3 bg-green-500 rounded-full"></button>
-          </div>
-          <nav className="space-y-5 mt-10">
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <HomeIcon /> Home
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <Search /> Search
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <Library />
-              Your Library
-            </a>
-          </nav>
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Hot Hits Brasil
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Rock Forever
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              This is Linkin Park
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Rock Classics
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Workout Motivation
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Classical Serenity
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Electronic Beats
-            </a>
-          </nav>
-        </aside>
+        <Sidebar />
         <main className="flex-1 p-6">
           <div className="flex items-center gap-3">
             <button className="rounded-full bg-black/40 p-1">
@@ -269,50 +218,7 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <Image
-            src="/hybrid-theory.jpg"
-            width={56}
-            height={56}
-            alt="Capa do álbum Hybrid Theory da Banda Linkin Park"
-          />
-          <div className="flex flex-col">
-            <strong className="font-normal">In the End</strong>
-            <span className="text-xs text-zinc-400">Linkin Park</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex items-center gap-6">
-            <Shuffle size={20} className="text-zinc-200" />
-            <SkipBack size={20} className="text-zinc-200" />
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
-              <Play />
-            </button>
-            <SkipForward size={20} className="text-zinc-200" />
-            <Repeat size={20} className="text-zinc-200" />
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-sx text-zinc-500">0:31</span>
-            <div className="h-1 rounded-full w-96 bg-zinc-600">
-              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
-            </div>
-            <span className="text-sx text-zinc-500">3:36</span>
-          </div>
-        </div>
-        <div className="flex items-center gap-2">
-          <Mic2 size={20} />
-          <LayoutList size={20} />
-          <Laptop2 size={20} />
-          <div className="flex items-center gap-2">
-            <Volume size={20} />
-            <div className="h-1 rounded-full w-24 bg-zinc-600">
-              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
-            </div>
-          </div>
-          <Maximize2 size={20} />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
